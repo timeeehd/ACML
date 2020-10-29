73b3e8ee9536d14181ba87cc75a99d6654ae7585
timeeehd <timeeehd@gmail.com>
2020-10-29 15:59:17 +0100

    Added seets file

--- selfStudyBP/main.py
+++ selfStudyBP/main.py
@@ -3,6 +3,7 @@ from math import exp
 from random import seed
 from random import random
 
+
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -14,22 +15,22 @@ def print_hi(name):
 
 def initialize_network(n_inputs, n_hidden, n_outputs):
     network = list()
-    hidden_layer = [{'weights':[random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
+    hidden_layer = [{'weights': [random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
     network.append(hidden_layer)
-    output_layer = [{'weights':[random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
+    output_layer = [{'weights': [random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
     network.append(output_layer)
     return network
 
 
 def activate(weights, inputs):
     activation = weights[-1]
-    for i in range(len(weights)-1):
-        activation += weights[i]*inputs[i]
+    for i in range(len(weights) - 1):
+        activation += weights[i] * inputs[i]
     return activation
 
 
 def transfer(activation):
-	return 1.0 / (1.0 + exp(-activation))
+    return 1.0 / (1.0 + exp(-activation))
 
 
 def forward_propagate(network, row):
@@ -44,12 +45,43 @@ def forward_propagate(network, row):
     return inputs
 
 
+def transfer_derivative(output):
+    return output * (1.0 - output)
+
+
+def backward_propagate_error(network, expected):
+    for i in reversed(range(len(network))):
+        layer = network[i]
+        errors = list()
+        if i != len(network) - 1:
+            for j in range(len(layer)):
+                error = 0.0
+                for neuron in network[i + 1]:
+                    error += (neuron['weights'][j] * neuron['delta'])
+                errors.append(error)
+        else:
+            for j in range(len(layer)):
+                neuron = layer[j]
+                errors.append(expected[j] - neuron['output'])
+        for j in range(len(layer)):
+            neuron = layer[j]
+            neuron['delta'] = errors[j] * transfer_derivative(neuron['output'])
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    seed(1)
-    network = initialize_network(2,1,2)
-    row = [1, 0, None]
-    output = forward_propagate(network, row)
-    print(output)
+    # seed(1)
+    # network = initialize_network(2, 1, 2)
+    # row = [1, 0, None]
+    # output = forward_propagate(network, row)
+    # print(output)
+    network = [
+        [{'output': 0.7105668883115941, 'weights': [0.13436424411240122, 0.8474337369372327, 0.763774618976614]}],
+        [{'output': 0.6213859615555266, 'weights': [0.2550690257394217, 0.49543508709194095]},
+         {'output': 0.6573693455986976, 'weights': [0.4494910647887381, 0.651592972722763]}]]
+    expected = [0, 1]
+    backward_propagate_error(network, expected)
+    for layer in network:
+        print(layer)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
